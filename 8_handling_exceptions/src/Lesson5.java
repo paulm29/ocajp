6aec1f27831898389064cecd338e07b365f0f590
paulm29 <paul.miscellaneous@gmail.com>
2016-07-08 10:31:29 +1000

    Add examples for 3 runtime exceptions

--- 8_handling_exceptions/src/Lesson5.java
+++ 8_handling_exceptions/src/Lesson5.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+
 /*
     8.5 Recognize common exception classes (such as NullPointerException,
     ArithmeticException, ArrayIndexOutOfBoundsException, ClassCastException)
@@ -30,10 +33,32 @@ public class Lesson5 {
         - ArrayIndexOutOfBoundsException: array not large enough for idnex
         - ClassCastException: inappropriate cast
         Others:
-        - ArrayStoreException: array cannot store type of object
+        - ArrayStoreException: array cannot store type of object. e.g. below
+        - NumberFormatException: when parsing values of incompatible types
      */
 
     public static void main(String[] args) {
 
+        /*
+            NullPointerException
+         */
+        List<Integer> heights = new ArrayList<>();
+        heights.add(null);
+        int h = heights.get(0);
+
+        /*
+            ArrayStoreException
+         */
+        String[] strings = { "stringValue" };
+        Object[] objects = strings;
+        String[] againStrings = (String[]) objects;
+        //againStrings[0] = new StringBuilder(); // DOES NOT COMPILE
+        objects[0] = new StringBuilder(); // ArrayStoreException
+
+        /*
+            NumberFormatException
+         */
+        int bad1 = Integer.parseInt("a"); // primitive
+        Integer bad2 = Integer.valueOf("123.45"); // Wrapper class
     }
 }
