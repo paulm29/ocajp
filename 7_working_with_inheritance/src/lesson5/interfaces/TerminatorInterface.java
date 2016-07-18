773afdd8dac59dda665e0cd730173dfa1bea6f14
paulm28 <paul.robotham@gmail.com>
2016-07-18 18:39:21 +1000

    Add rules for abstract classes

--- 7_working_with_inheritance/src/lesson5/interfaces/TerminatorInterface.java
+++ 7_working_with_inheritance/src/lesson5/interfaces/TerminatorInterface.java
@@ -36,6 +36,15 @@ public interface TerminatorInterface extends RobotInterface {
     /*
         New in Java 8
         Default methods are similar to concrete methods in abstract classes
+        Rules:
+        1. A default method may only be declared within an interface and not within a
+        class or abstract class.
+        2. A default method must be marked with the default keyword. If a method is marked
+         as default, it must provide a method body.
+        3. A default method is not assumed to be static, final, or abstract, as it may be
+         used or overridden by a class that implements the interface.
+        4. Like all methods in an interface, a default method is assumed to be public and
+         will not compile if marked as private or protected.
      */
     default void makeScaryFace() {
         System.out.println(">:");
